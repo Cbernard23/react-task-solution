b5fbc76cb2eafbcb92ffde10a49db12695e49568
Chris Bernard <chris@harvestprofit.com>
2018-07-18 11:33:16 -0500

    added login and signup

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import MyComponent from './MyComponent';
+import Login from './Login';
+import Signup from './Signup'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<MyComponent />, document.getElementById('myComponent'))
+ReactDOM.render(<MyComponent />, document.getElementById('myComponent'));
+ReactDOM.render(<Login />, document.getElementById('login'));
+ReactDOM.render(<Signup />, document.getElementById('signup'));
 registerServiceWorker();
